d81b90a27502cbaf680902e0fd564607c99eb547
cygus84 <cygus84@gmail.com>
2024-03-12 09:17:51 +0000

    ac
    ac

---
+++ Section006/src/app/Start.java
@@ -0,0 +1,14 @@
+package app;
+
+import app.numberInWord.NumberInWord;
+
+public class Start {
+
+	public static void main(String[] args) {
+		System.out.println("Sekcja 06 Control Flow");
+	
+		NumberInWord numberIn = new NumberInWord();
+		numberIn.printNumberInWord(7); // Done!
+	}
+
+}
